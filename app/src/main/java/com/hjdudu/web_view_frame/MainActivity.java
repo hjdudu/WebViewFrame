8615b565bb57953e1779d2b8eee73853e6f54f30
peiyangfan <peiyangfan@souche.com>
2020-11-17 09:29:08 +0800

    init

---
+++ app/src/main/java/com/hjdudu/web_view_frame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hjdudu.web_view_frame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
